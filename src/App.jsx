00c76b4caefe7503b5ce5dec357cdfef14eafd18
RemKiovo <epaulais.remi@gmail.com>
2024-08-30 11:54:06 +0200

    App Redirects to user page + changed some unwanted text interactions

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,12 @@
-import { useEffect, useState } from 'react'
-import getUser from './services/user.service'
-import getActivity from './services/activity.service'
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
-	const [user, setUser] = useState(null)
-	const [userActivity, setUserActivity] = useState(null)
-
 	useEffect(() => {
-		const fetchUserData = async () => {
-			const user = await getUser(12)
-			const userActivity = await getActivity(12)
-
-			setUser(user.data)
-			setUserActivity(userActivity.data)
-		}
-
-		fetchUserData()
+		window.location.pathname = '/user/12'
 	}, [])
 
-	if (!user) return <h1>Loading...</h1>
-
-	return (
-		<main>
-			<h1>Hello, World!</h1>
-			<h2>API</h2>
-			<h3 className='font-bold text-xl'>Get User</h3>
-			<section className='flex gap-4'>
-				<p>{user.id}</p>
-				<p>{user.userInfos.firstName}</p>
-				<p>{user.userInfos.lastName}</p>
-				<p>{user.userInfos.age}</p>
-			</section>
-
-			<h3 className='font-bold text-xl'>Get Activity</h3>
-			<section className='text-red-500 flex gap-4 w-full'>
-				{userActivity.sessions.map((session, index) => (
-					<div key={index} className='flex-1'>
-						<p>Day: {session.day}</p>
-						<p>SessionLength: {session.sessionLength}</p>
-						<p>KG: {session.kilogram}</p>
-						<p>Calories: {session.calories}</p>
-					</div>
-				))}
-			</section>
-		</main>
-	)
+	return <main></main>
 }
 
 export default App
